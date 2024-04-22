92d0800712276975dd7e092113b5874419cb3842
hangyu cho <chg3376@gmail.com>
2024-04-23 00:43:02 +0900

    fix : add storybook and add diary

---
+++ frontend/src/components/diary/DiaryHeader.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'DiaryHeader'
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/diary/calendar/CalendarRow.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import CalendarItem from '@/components/diary/calendar/CalendarItem.vue'
+
+export default defineComponent({
+  name: 'CalendarRow',
+  data() {
+    return {
+      today: new Date().toISOString().slice(0, 10),
+      dates: Array.from({ length: 7 }, (_, i) => {
+        const date = new Date()
+        date.setDate(date.getDate() + i)
+        return date.toISOString().slice(0, 10)
+      }),
+      mode: '평범'
+    }
+  },
+  components: {
+    CalendarItem
+  }
+})
+</script>
+
+<template>
+  <div>
+    <div class="flex justify-between">
+      <div class="flex">
+        <CalendarItem v-for="date in dates" :key="date" :date="date" :isToday="date === today" :mode="mode" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
