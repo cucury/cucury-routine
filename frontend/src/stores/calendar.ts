1ae672b793e00e6b804feef0e89e7b06fb9cb276
hangyu cho <chg3376@gmail.com>
2024-04-29 12:31:39 +0900

    fix : apply prev and next month for calendar

--- frontend/src/stores/calendar.ts
+++ frontend/src/stores/calendar.ts
@@ -3,35 +3,52 @@ import { getCalendarMonth } from 'mnth'
 import type { DiaryProps } from '@/props'
 
 export const useCalendarStore = defineStore('calendar', {
-  state: () => ({
-    currentDate: new Date(),
+  state: (): { currentMonth: Date; currentDate: Date } => ({
+    currentMonth: new Date(),
+    currentDate: new Date()
   }),
   actions: {
-    setDate(date: Date) {
-      this.currentDate = new Date(date.getFullYear(), date.getMonth(), 1)
+    movePrev() {
+      const prev = new Date(this.currentMonth)
+      prev.setMonth(prev.getMonth(), 0)
+      this.setMonth(prev)
+    },
+    moveNext() {
+      const next = new Date(this.currentMonth)
+      next.setMonth(next.getMonth() + 2, 0)
+      this.setMonth(next)
+    },
+    getMonth() {
+      return this.currentMonth
+    },
+    setMonth(month: Date) {
+      this.currentMonth = new Date(month.getFullYear(), month.getMonth(), 1)
     },
     getDate() {
       return this.currentDate
     },
+    setDate(date: Date) {
+      this.currentDate = new Date(date.getFullYear(), date.getMonth(), 1)
+    },
     getWeeks() {
-      const date = new Date()
+      const date = this.getMonth()
       const weeks = getCalendarMonth(date, { firstDayOfWeek: 0 }).map((week) =>
         week.map((day) => day.getTime())
       )
       return weeks.map((times) => {
         return times.map((time) => {
-          const result:DiaryProps = {
+          const result: DiaryProps = {
             id: -1,
             mode: '',
             content: '',
             time: time,
-            createdAt: 0,
+            createdAt: 0
           }
           return result
         })
       })
     }
-  },
+  }
 })
 
 export default useCalendarStore
