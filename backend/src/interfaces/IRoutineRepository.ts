f30899058be262bd3b9230064c69d40af4d3bde0
hangyuCho <chg3376@gmail.com>
2024-06-10 09:36:42 +0900

    add remove routine

--- backend/src/interfaces/IRoutineRepository.ts
+++ backend/src/interfaces/IRoutineRepository.ts
@@ -1,8 +1,9 @@
 import { Routine } from '../entities/Routine'
 
 export interface IRoutineRepository {
-  create(diary: Routine): Promise<Routine>
-  update(diary: Routine): Promise<Routine>
+  create(routine: Routine): Promise<Routine>
+  update(routine: Routine): Promise<Routine>
   find(userHash: string): Promise<Routine[]>
   findById(id: number, userHash: string): Promise<Routine>
+  delete(routine: Routine): Promise<Routine>
 }
--- frontend/src/components/routine/CucuryRoutineDetail.vue
+++ frontend/src/components/routine/CucuryRoutineDetail.vue
@@ -26,7 +26,7 @@
             class='pl-6 relative'
             :key='j' v-for='(el,j) in v.routines'>
             <div class='hover:opacity-50 absolute h-full w-[90%] -left-2 bg-white opacity-0 flex items-center'>
-              <button type='button'>
+              <button type='button' @click='removeRoutine(el.id)'>
                 <svg class="w-6 h-6 text-lime-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                   <path fill-rule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clip-rule="evenodd"/>
                 </svg>
@@ -128,6 +128,18 @@ export default {
       // fetch data from server
       const res = await this.routineServer.get(`/routine/completed-dt/${this.currentDt}`)
       this.groupedRoutines = res.data
+    },
+    async removeRoutine(id) {
+      try {
+        await this.routineServer.delete(`/routine/${id}`)
+        this.groupedRoutines = this.groupedRoutines.map(el => {
+            el.routines = el.routines.filter(r => r.id !== id)
+            return el
+          })
+      } catch (e) {
+        console.error(e)
+      }
+
     },
     openNewRoutine() {
       this.detailModalRef.isShow = true
