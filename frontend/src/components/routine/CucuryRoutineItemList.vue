5704546d5241e9a239514e43c9f4d18ad807b33e
hangyuCho <chg3376@gmail.com>
2024-06-11 07:53:48 +0900

    fix : refactoring

--- frontend/src/components/routine/CucuryRoutineItemList.vue
+++ frontend/src/components/routine/CucuryRoutineItemList.vue
@@ -1,20 +1,45 @@
 <template>
-  {{ new Date(currentTime).toLocaleDateString() }}
+  <div class='flex items-center gap-2 relative pt-2'>
+    <div class='flex items-center'>
+      {{ new Date(currentTime).toLocaleDateString() }}
+    </div>
+    <a
+      v-if='new Date(currentTime).toLocaleDateString("sv-SE") !== new Date().toLocaleDateString("sv-SE")'
+      class='bg-black text-white p-1 rounded-lg flex items-center gap-1 cursor-pointer absolute right-[-4.4rem]'
+      @click='() => {
+        this.currentTime = new Date().getTime()
+        this.weeks = this.getWeeks(this.currentTime)
+        this.$emit("update:current-time", this.currentTime)
+      }'
+    >
+      <div>
+        <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m16 10 3-3m0 0-3-3m3 3H5v3m3 4-3 3m0 0 3 3m-3-3h14v-3"/>
+        </svg>
+      </div>
+      <div>
+        Today︎
+      </div>
+    </a>
+  </div>
   <div class='flex gap-2 items-center'>
     <button type='button' @click='prevWeek'>
       👈
     </button>
     <div class='flex flex-col gap-2'>
       <div class='flex gap-2'>
+
         <CucuryRoutineItem
           :key='i'
           v-for='(v,i) in weeks'
           :time='v'
-          :isSelected='v === currentTime'
+          :isSelected='Number(v) === Number(currentTime)'
           @click='() => {
             this.currentTime = new Date(v).getTime()
             this.$emit("update:current-time", this.currentTime)
-           }' />
+           }' >
+          v: {{Number(v) === Number(currentTime)}}
+        </CucuryRoutineItem>
       </div>
     </div>
     <button type='button' @click='nextWeek'>
@@ -64,19 +89,14 @@ export default {
       this.$emit("update:current-time", this.currentTime)
       this.weeks = this.getWeeks(nextDate.getTime())
     },
-    getWeeks(time: number) {
+    getWeeks(time) {
       return getCalendarMonth(new Date(time), { firstDayOfWeek: 0 } )
         .filter(el => {
-          const currentDate = new Date(time)
-          currentDate.setHours(0, 0, 0, 0)
-          const currentWeek = el
-            .map(e => {
-              const currentDate = new Date(e)
-              currentDate.setHours(0, 0, 0, 0)
-              return currentDate
-            })
-            .filter(e => e.getTime() === currentDate.getTime()
-            )
+          const currentDate = new Date(time).toLocaleDateString("sv-SE")
+          const currentWeek =
+            el
+            .map(e => new Date(e).toLocaleDateString("sv-SE"))
+            .filter(e => e === currentDate)
           return currentWeek.length > 0
         })[0].map(e => e.getTime())
     }
